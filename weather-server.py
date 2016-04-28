4cc28f5817c79b7d553651dc32812bc07f393c51
salsa-system <admin@salsa-system.com>
2016-04-28 11:08:38 +0200

    Forecast weather added

--- weather-server.py
+++ weather-server.py
@@ -26,8 +26,9 @@ payment = Payment(app, wallet)
 
 SERVER_PORT = '5050'
 
-OWMAPIKEY = os.environ.get('OPEN_WEATHER_MAP_API_KEY')
-OWMURL = 'http://api.openweathermap.org/data/2.5/weather'
+OWM_API_KEY = os.environ.get('OPEN_WEATHER_MAP_API_KEY')
+OWM_CURRENT_URL = 'http://api.openweathermap.org/data/2.5/'
+OWM_MODES= ['weather', 'forecast']
 
 # Provide the app manifest to the 21 crawler.
 @app.route('/weather/manifest')
@@ -41,10 +42,10 @@ def manifest():
 def client():
    return send_from_directory('static', 'weather.py')
 
-# Charge a fixed fee per request to the /city weather endpoint
-@app.route('/weather/city')
+# Charge a fixed fee per request to the /city current weather endpoint
+@app.route('/weather/current/city')
 @payment.required(2500)
-def city_weather():
+def current_city_weather():
    # the parameters sent by the client
    cityname = str(request.args.get('cityname'))
    countrycode = str(request.args.get('countrycode'))
@@ -66,15 +67,15 @@ def city_weather():
    else:
       langparam = "en"
 
-   userdata = {"q": cityparams, "units": unitparam, "lang": langparam, "APPID": OWMAPIKEY}
-   resp = requests.get(OWMURL, params=userdata)
+   userdata = {"q": cityparams, "units": unitparam, "lang": langparam, "APPID": OWM_API_KEY}
+   resp = requests.get(OWM_CURRENT_URL + OWM_MODES[0], params=userdata)
 
    return resp.text
 
-# Charge a fixed fee per request to the /zip weather endpoint
-@app.route('/weather/zip')
+# Charge a fixed fee per request to the /zip current weather endpoint
+@app.route('/weather/current/zip')
 @payment.required(2500)
-def zip_weather():
+def current_zip_weather():
    # the parameters sent by the client
    zipcode = str(request.args.get('zipcode'))
    countrycode = str(request.args.get('countrycode'))
@@ -93,15 +94,15 @@ def zip_weather():
    else:
       langparam = "en"
 
-   userdata = {"zip": cityparams, "units": unitparam, "lang": langparam, "APPID": OWMAPIKEY}
-   resp = requests.get(OWMURL, params=userdata)
+   userdata = {"zip": cityparams, "units": unitparam, "lang": langparam, "APPID": OWM_API_KEY}
+   resp = requests.get(OWM_CURRENT_URL + OWM_MODES[0], params=userdata)
 
    return resp.text
 
-# Charge a fixed fee per request to the /geo weather endpoint
-@app.route('/weather/geo')
+# Charge a fixed fee per request to the /geo current weather endpoint
+@app.route('/weather/current/geo')
 @payment.required(2500)
-def geo_weather():
+def current_geo_weather():
    # the parameters sent by the client
    latitude = request.args.get('latitude')
    longitude = request.args.get('longitude')
@@ -118,8 +119,90 @@ def geo_weather():
    else:
       langparam = "en"
 
-   userdata = {"lat": latitude, "lon": longitude, "units": unitparam, "lang": langparam, "APPID": OWMAPIKEY}
-   resp = requests.get(OWMURL, params=userdata)
+   userdata = {"lat": latitude, "lon": longitude, "units": unitparam, "lang": langparam, "APPID": OWM_API_KEY}
+   resp = requests.get(OWM_CURRENT_URL + OWM_MODES[0], params=userdata)
+
+   return resp.text
+
+# Charge a fixed fee per request to the /city forecast weather endpoint
+@app.route('/weather/forecast/city')
+@payment.required(2500)
+def forecast_city_weather():
+   # the parameters sent by the client
+   cityname = str(request.args.get('cityname'))
+   countrycode = str(request.args.get('countrycode'))
+   unit = str(request.args.get('unit'))
+   lang = str(request.args.get('lang'))
+
+   if countrycode:
+      cityparams = cityname + ',' + countrycode
+   else:
+      cityparams = cityname
+
+   if unit:
+      unitparam = unit
+   else:
+      unitparam = "standard"
+
+   if lang:
+      langparam = lang
+   else:
+      langparam = "en"
+
+   userdata = {"q": cityparams, "units": unitparam, "lang": langparam, "APPID": OWM_API_KEY}
+   resp = requests.get(OWM_CURRENT_URL + OWM_MODES[1], params=userdata)
+
+   return resp.text
+
+# Charge a fixed fee per request to the /zip forecast weather endpoint
+@app.route('/weather/forecast/zip')
+@payment.required(2500)
+def forecast_zip_weather():
+   # the parameters sent by the client
+   zipcode = str(request.args.get('zipcode'))
+   countrycode = str(request.args.get('countrycode'))
+   unit = str(request.args.get('unit'))
+   lang = str(request.args.get('lang'))
+
+   cityparams = zipcode + ',' + countrycode
+
+   if unit:
+      unitparam = unit
+   else:
+      unitparam = "standard"
+
+   if lang:
+      langparam = lang
+   else:
+      langparam = "en"
+
+   userdata = {"zip": cityparams, "units": unitparam, "lang": langparam, "APPID": OWM_API_KEY}
+   resp = requests.get(OWM_CURRENT_URL + OWM_MODES[1], params=userdata)
+
+   return resp.text
+
+# Charge a fixed fee per request to the /geo forecast weather endpoint
+@app.route('/weather/forecast/geo')
+@payment.required(2500)
+def forecast_geo_weather():
+   # the parameters sent by the client
+   latitude = request.args.get('latitude')
+   longitude = request.args.get('longitude')
+   unit = str(request.args.get('unit'))
+   lang = str(request.args.get('lang'))
+
+   if unit:
+      unitparam = unit
+   else:
+      unitparam = "standard"
+
+   if lang:
+      langparam = lang
+   else:
+      langparam = "en"
+
+   userdata = {"lat": latitude, "lon": longitude, "units": unitparam, "lang": langparam, "APPID": OWM_API_KEY}
+   resp = requests.get(OWM_CURRENT_URL + OWM_MODES[1], params=userdata)
 
    return resp.text
 
--- weather.py
+++ weather.py
@@ -8,31 +8,39 @@ from two1.bitrequests import BitTransferRequests
 SERVER_IP_ADDRESS='10.244.132.211'
 SERVER_PORT='5050'
 
-# Configure your Bitcoin wallet. 
+MODE_LIST= ['current', 'forecast']
+
+# Configure your Bitcoin wallet.
 username = Config().username
 wallet = Wallet()
 requests = BitTransferRequests(wallet, username)
 
 # Send city name, country code, unit and language to the endpoint
-def send_city(cityname, countrycode, unit, language):
+def send_city(mode, cityname, countrycode, unit, language):
     # Display info the user is sending
+    print('Mode: {0}'.format(mode))
     print('City: {0}'.format(cityname))
     print('Country: {0}'.format(countrycode))
     print('Unit: {0}'.format(unit))
     print('Language: {0}'.format(language))
 
     # 402-payable endpoint URL and request
-    if countrycode:
-       weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + '/weather/city?cityname={0}&countrycode={1}'
+    if mode == MODE_LIST[0]:
+        route_url = '/weather/current/'
     else:
-       weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + '/weather/city?cityname={0}'
+        route_url = '/weather/forecast/'
+
+    if countrycode:
+       weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + route_url + 'city?cityname={0}&countrycode={1}'
+    else:
+       weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + route_url + 'city?cityname={0}'
 
     if unit:
        weather_url = weather_url + '&unit={2}'
-    
+
     if language:
        weather_url = weather_url + '&lang={3}'
-    
+
     response = requests.get(url=weather_url.format(cityname, countrycode, unit, language))
 
     print('URL: ' + response.url)
@@ -40,22 +48,28 @@ def send_city(cityname, countrycode, unit, language):
     print(response.text)
 
 # Send zip code, country code, unit and language to the endpoint
-def send_zip(zipcode, countrycode, unit, language):
+def send_zip(mode, zipcode, countrycode, unit, language):
     # tell the user what info they're sending
+    print('Mode: {0}'.format(mode))
     print('Zip: {0}'.format(zipcode))
     print('Country: {0}'.format(countrycode))
     print('Unit: {0}'.format(unit))
     print('Language: {0}'.format(language))
 
     # 402-payable endpoint URL and request
-    weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + '/weather/zip?zipcode={0}&countrycode={1}'
+    if mode == MODE_LIST[0]:
+        route_url = '/weather/current/'
+    else:
+        route_url = '/weather/forecast/'
+
+    weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + route_url + 'zip?zipcode={0}&countrycode={1}'
 
     if unit:
        weather_url = weather_url + '&unit={2}'
-    
+
     if language:
        weather_url = weather_url + '&lang={3}'
-    
+
     response = requests.get(url=weather_url.format(zipcode, countrycode, unit, language))
 
     print('URL: ' + response.url)
@@ -63,22 +77,28 @@ def send_zip(zipcode, countrycode, unit, language):
     print(response.text)
 
 # Send latitude, longitude, unit and language to the endpoint
-def send_geo(latitude, longitude, unit, language):
+def send_geo(mode, latitude, longitude, unit, language):
     # tell the user what info they're sending
+    print('Mode: {0}'.format(mode))
     print('Latitude: {0}'.format(latitude))
     print('Longitude: {0}'.format(longitude))
     print('Unit: {0}'.format(unit))
     print('Language: {0}'.format(language))
 
     # 402-payable endpoint URL and request
-    weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + '/weather/geo?latitude={0}&longitude={1}'
+    if mode == MODE_LIST[0]:
+        route_url = '/weather/current/'
+    else:
+        route_url = '/weather/forecast/'
+
+    weather_url = 'http://' + SERVER_IP_ADDRESS + ':' + SERVER_PORT + route_url + 'geo?latitude={0}&longitude={1}'
 
     if unit:
        weather_url = weather_url + '&unit={2}'
-    
+
     if language:
        weather_url = weather_url + '&lang={3}'
-    
+
     response = requests.get(url=weather_url.format(latitude, longitude, unit, language))
 
     print('URL: ' + response.url)
@@ -87,15 +107,18 @@ def send_geo(latitude, longitude, unit, language):
 
 # Read args from command line.
 # send_city():
-# python3 weather-client.py "London" "uk" "" ""
-# python3 weather-client.py "London" "uk" "metric" "fr"
+# python3 weather.py "current" "London" "uk" "" ""
+# python3 weather.py "current" "London" "uk" "metric" "fr"
+# python3 weather.py "forecast" "London" "uk" "" ""
+# python3 weather.py "forecast" "London" "uk" "metric" "fr"
 # send_zip():
-# python3 weather-client.py "94040" "us" "imperial" "es"
+# python3 weather.py "current" "94040" "us" "imperial" "es"
+# python3 weather.py "forecast" "94040" "us" "imperial" "es"
 # seng_geo():
-# python3 weather-client.py "51.51" "-0.13" "" ""
+# python3 weather.py "current" "51.51" "-0.13" "" ""
+# python3 weather.py "forecast" "51.51" "-0.13" "" ""
 if __name__ == '__main__':
     from sys import argv
-    send_city(argv[1], argv[2], argv[3], argv[4])
-    #send_zip(argv[1], argv[2], argv[3], argv[4])
-    #send_geo(argv[1], argv[2], argv[3], argv[4])
-
+    send_city(argv[1], argv[2], argv[3], argv[4], argv[5])
+    #send_zip(argv[1], argv[2], argv[3], argv[4], argv[5])
+    #send_geo(argv[1], argv[2], argv[3], argv[4], argv[5])
